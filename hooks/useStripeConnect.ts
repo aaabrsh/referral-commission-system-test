b200faadc0f19217ef2e955ce8e8b1f3cc85be5e
aaabrham <aaabrham421@gmail.com>
2025-09-05 14:50:04 +0300

    minor update

--- hooks/useStripeConnect.ts
+++ hooks/useStripeConnect.ts
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import api, { ApiRoutes } from "@/lib/api";
 import { toast } from "sonner";
 import { redirect } from "next/navigation";
